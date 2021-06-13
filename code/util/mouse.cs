3c44d2f2d0d119946ae2824597522b1dbccf80d6
Pivius <pivisens@gmail.com>
2021-06-13 20:15:09 +0200

    Moved mouse convars

--- code/util/mouse.cs
+++ code/util/mouse.cs
@@ -7,17 +7,6 @@ namespace Core
 {
 	public static class MouseInput
 	{
-		[ConVar.ClientData( "m_customaccel" )] public static float CustomAccel { get; set; } = 0.0f;
-		[ConVar.ClientData( "m_customaccel_scale" )] public static float CustomAccelScale { get; set; } = 0.04f;
-		[ConVar.ClientData( "m_customaccel_max" )] public static float CustomAccelMax { get; set; } = 0.0f;
-		[ConVar.ClientData( "m_customaccel_exponent" )] public static float CustomAccelExponent { get; set; } = 1.0f;
-		[ConVar.ClientData( "m_rawinput" )] public static bool RawInput { get; set; } = true;
-		[ConVar.ClientData("m_mouseenable")] public static bool MouseEnable { get; set; } = true;
-		[ConVar.ClientData("m_filter")] public static bool MouseFilter { get; set; } = true;
-		[ConVar.ClientData("sensitivity")] public static float Sensitivity { get; set; } = 1.0f;
-		[ConVar.ClientData("m_yaw")] public static float Yaw { get; set; } = 0.022f;
-		[ConVar.ClientData("m_pitch")] public static float Pitch { get; set; } = 0.022f;
-		
 		public static float GetConCommand(string command)
 		{
 			return Single.Parse(ConsoleSystem.GetValue(command));
