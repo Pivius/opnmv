cc8a984fb010a7841453fa61b0c1c1325ab53fe3
Pivius <pivisens@gmail.com>
2021-06-13 20:44:17 +0200

    Fixed error

--- code/player/convars.cs
+++ code/player/convars.cs
@@ -9,9 +9,9 @@ namespace Core
 		[ConVar.ClientData( "m_customaccel_scale" )] public static float CustomAccelScale { get; set; } = 0.04f;
 		[ConVar.ClientData( "m_customaccel_max" )] public static float CustomAccelMax { get; set; } = 0.0f;
 		[ConVar.ClientData( "m_customaccel_exponent" )] public static float CustomAccelExponent { get; set; } = 1.0f;
-		[ConVar.ClientData( "m_rawinput" )] public static bool RawInput { get; set; } = true;
-		[ConVar.ClientData("m_mouseenable")] public static bool MouseEnable { get; set; } = true;
-		[ConVar.ClientData("m_filter")] public static bool MouseFilter { get; set; } = true;
+		[ConVar.ClientData( "m_rawinput" )] public static float RawInput { get; set; } = 1;
+		[ConVar.ClientData("m_mouseenable")] public static float MouseEnable { get; set; } = 1;
+		[ConVar.ClientData("m_filter")] public static float MouseFilter { get; set; } = 1;
 		[ConVar.ClientData("sensitivity")] public static float Sensitivity { get; set; } = 1.0f;
 		[ConVar.ClientData("m_yaw")] public static float Yaw { get; set; } = 0.022f;
 		[ConVar.ClientData("m_pitch")] public static float Pitch { get; set; } = 0.022f;
